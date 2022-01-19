50063d4ede48db8560a7b0e43bd61455d3a4caeb
zzq89757 <1604653835@qq.com>
2022-01-19 21:51:05 +0800

    fisrt by ak

---
+++ src/components/Simple.vue
@@ -0,0 +1,38 @@
+<!-- 快速克隆组件 -->
+<template>
+  <div>
+      <div class="simple">
+          <img src="https://vuejs.bootcss.com/images/lifecycle.png" alt="ass"/>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  // components: {},
+
+  // computed: {},
+
+  // mounted: {},
+
+  // methods: {},
+};
+</script>
+<style  scoped>
+*{
+    margin: 0;
+    padding: 0;
+}
+.simple img{
+    height: 100%;
+    width: 100%;
+    /* background-color: brown; */
+    /* background-image: url("https://vuejs.bootcss.com/images/lifecycle.png"); */
+    /* background-repeat: no-repeat; */
+    /* background-size: cover; */
+}
+</style>
