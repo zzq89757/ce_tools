823be7f0a0899463d956b020d78020a66b5d66cb
zzq89757 <1604653835@qq.com>
2022-01-19 13:14:22 +0800

    change router and css

--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,45 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import MultiFragment from '@/components/MultiFragment'
-import Simple from '@/components/Simple'
-import Multipoint from '@/components/Multipoint'
-import Tmcal from '@/components/Tmcal'
-import DNAsequence from '@/components/DNAsequence'
-import Restructure from '@/components/Restructure'
+const MultiFragment = () => import('@/views/MultiFragment')
+const Multipoint = () => import('@/views/Multipoint')
+const Simple = () => import('@/views/Simple')
+const Tmcal = () => import('@/views/Tmcal')
+const DNAsequence = () => import('@/views/DNAsequence')
+const Restructure = () => import('@/views/Restructure')
 
 Vue.use(Router)
-
+const routes = [
+  {
+    path: '/',
+    redirect: '/simple',
+    name: 'App',
+  }, {
+    path: '/multifragment',
+    name: 'MultiFragment',
+    component: MultiFragment
+  }, {
+    path: '/simple',
+    name: 'simple',
+    component: Simple
+  }, {
+    path: '/multipoint',
+    name: 'multipoint',
+    component: Multipoint
+  }, {
+    path: '/restructure',
+    name: 'restructure',
+    component: Restructure
+  }, {
+    path: '/dnasuquence',
+    name: 'dnasuquence',
+    component: DNAsequence
+  }, {
+    path: '/tmcal',
+    name: 'tmcal',
+    component: Tmcal
+  }
+]
 export default new Router({
-  mode:'history',
-  routes: [
-    // mode: 'history',
-    {
-      path: '/',
-      // redirect:'/multifragment',
-      redirect:'/simple',
-      name: 'App',
-      mode:"history"
-      // component: App
-    },{
-      path:'/multifragment',
-      name:'MultiFragment',
-      component:MultiFragment
-    },{
-      path:'/simple',
-      name:'simple',
-      mode:"history",
-      component:Simple
-    },{
-      path:'/multipoint',
-      name:'multipoint',
-      component:Multipoint
-    },{
-      path:'/restructure',
-      name:'restructure',
-      component:Restructure
-    },{
-      path:'/dnasuquence',
-      name:'dnasuquence',
-      component:DNAsequence
-    },{
-      path:'/tmcal',
-      name:'tmcal',
-      component:Tmcal
-    }
-  ]
+  mode: 'history',
+  routes
 })
