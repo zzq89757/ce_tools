b38a04bcd9fa1eb1970c590d2636bb0a9f458d5b
zzq89757 <1604653835@qq.com>
2022-01-29 00:30:16 +0800

    2/3 finished

--- src/views/Simple.vue
+++ src/views/Simple.vue
@@ -1,24 +1,65 @@
 <!-- 快速克隆组件 -->
 <template>
   <div>
-    <Input :message="tips"></Input>
+    <div>
+      <Input :message="tips"></Input>
+      <Input class="input_shortseq input2" :message="cut_message">
+        <div class="check_line">
+          <div>
+            <select name="cut1" id="cut1">
+              <option value="none">不增加酶切位点</option>
+              <option
+                :value="item.key"
+                v-for="item in jsondata"
+                :key="item.value"
+              >
+                {{ item.value }}
+              </option>
+            </select>
+            <select name="cut2" id="cut2">
+              <option value="none">不增加酶切位点</option>
+              <option
+                :value="item.key"
+                v-for="item in jsondata"
+                :key="item.value"
+              >
+                {{ item.value }}
+              </option>
+            </select>
+          </div>
+        </div>
+      </Input>
+      <Button></Button>
+    </div>
+    <!-- 图谱预留空间 -->
+    <div>
+      
+    </div>
   </div>
 </template>
 
 <script>
 import Input from "@/components/content/input/Input.vue";
+import Button from "@/components/content/button/Button";
+import jsondata from "@/assets/js/select.json";
 export default {
   data() {
     return {
+      jsondata,
       tips: {
         num: 1,
         message: "插入片段",
         tips: "至少50bp（自动过滤非AGCT字符）",
       },
+      cut_message: {
+        num: 2,
+        message: "酶切位点",
+        tips: "至少50bp（自动过滤非AGCT字符）",
+      },
     };
   },
 
-  components: { Input },
+  components: { Input, Button },
 
   // computed: {},
 
