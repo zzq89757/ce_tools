e45d6dad8ec3d86e3cb74e9e8b2da776eb8e0a7e
zzq89757 <1604653835@qq.com>
2022-01-29 17:41:27 +0800

    change restructure

--- src/views/Restructure.vue
+++ src/views/Restructure.vue
@@ -1,6 +1,12 @@
 <!--  -->
 <template>
-  <container><div></div></container>
+  <container>
+    <div>
+      <Input :message="message1"> </Input>
+      <Input :message="message2"> </Input>
+      <Input :message="message3"> </Input>
+    </div>
+  </container>
 </template>
 
 <script>
@@ -8,10 +14,26 @@ import Input from "@/components/content/input/Input";
 import Container from "@/components/content/container/Container";
 export default {
   data() {
-    return {};
+    return {
+      message1: {
+        num: 1,
+        message: "实验类型",
+        tips: "",
+      },
+      message2: {
+        num: 2,
+        message: "具体实验类型",
+        tips: "",
+      },
+      message3: {
+        num: 3,
+        message: "序列长度",
+        tips: "",
+      },
+    };
   },
 
-  components: { Input,Container },
+  components: { Input, Container },
 
   // computed: {},
 
