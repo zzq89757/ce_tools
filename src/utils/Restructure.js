1ca679e2c7e2543c1cbe50bada54f1d535432217
zzq89757 <1604653835@qq.com>
2022-02-09 21:29:39 +0800

    remove dubug log

--- src/utils/Restructure.js
+++ src/utils/Restructure.js
@@ -10,7 +10,7 @@ function simpleClone(insert) {
 function singleClone(load, insert) {
     return { loadPay: getLoadPay(load), insertpay: getInsertPay(insert) };
 }
-console.log(singleClone(12, 12));
+
 
 //多片段克隆
 function multiFragment(load, ...inserts) {
@@ -21,18 +21,15 @@ function multiFragment(load, ...inserts) {
     return { loadpay: getLoadPay(load), insertpay: ary }
 }
 
-console.log(multiFragment(10, 10, 10));
 
 //单点突变
 function singlePoint(mutload) {
     return getLoadPay(mutload, 400);
 }
-console.log(singlePoint(12));
 //双位点突变
 function doublePoint(point1, point2) {
     return { point1: getMuitlInsertPay(point1), point2: getInsertPay(point2) };
 }
-console.log(doublePoint(1, 1));
 //多点突变
 //插入片段数目不确定 使用不定参数
 function muitlPoint(...args) {
